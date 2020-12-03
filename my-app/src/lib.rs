d4321c75d8b052c5d427daa55cc398a02fc08706
r-tezuka <ryuta.tezuka2@gmail.com>
2020-12-04 03:25:34 +0900

    複数の点列の表示を実装

--- my-app/src/lib.rs
+++ my-app/src/lib.rs
@@ -1,4 +1,4 @@
-use std::{ rc::Rc, cell::Cell };
+use std::{ rc::Rc, cell::{Cell, RefCell} };
 use wasm_bindgen::prelude::*;
 use wasm_bindgen::JsCast;
 
@@ -18,24 +18,34 @@ pub fn start() -> Result<(), JsValue> {
         .dyn_into::<web_sys::CanvasRenderingContext2d>()?;
     let context = Rc::new(context);
     let dragged = Rc::new(Cell::new(false));
-
-    let x = Rc::new(Cell::new(10.0));
-    let y = Rc::new(Cell::new(10.0));
+    let target = Rc::new(Cell::new([0.0, 0.0]));
     let point_width = 10.0;
     let w = Rc::new(point_width);
-    context.fill_rect(x.get() - *w/2.0, y.get() - *w/2.0, *w, *w);
+    let points = vec![[10.0, 10.0],[100.0, 10.0],[200.0, 10.0]];
+    for p in points.iter() {
+        context.fill_rect(p[0] - *w/2.0, p[1] - *w/2.0, *w, *w);
+    }
+    let points = Rc::new(RefCell::new(points));
     {
         let context = context.clone();
         let dragged = dragged.clone();
-        let x = x.clone();
-        let y = y.clone();
         let w = w.clone();
+        let points = points.clone();
         let closure = Closure::wrap(Box::new(move |event: web_sys::MouseEvent| {
             context.begin_path();
             let mouse_x = event.offset_x() as f64;
             let mouse_y = event.offset_y() as f64;
-            if mouse_x - *w/2.0 < x.get() && x.get() <  mouse_x + *w/2.0 && mouse_y - *w/2.0 < y.get() && y.get() < mouse_y + *w/2.0 {
-                dragged.set(true);
+            let mut id: Option<usize> = None;
+            for (i, p) in points.borrow().iter().enumerate() {
+                if mouse_x - *w/2.0 < p[0] && p[0] <  mouse_x + *w/2.0 && mouse_y - *w/2.0 < p[1] && p[1] < mouse_y + *w/2.0 {
+                    dragged.set(true);
+                    id = Some(i);
+                }else{
+                    context.fill_rect(p[0] - *w/2.0, p[1] - *w/2.0, *w, *w);
+                }
+            }
+            if id != None {
+                points.borrow_mut().remove(id.unwrap());
             }
         }) as Box<dyn FnMut(_)>);
         canvas.add_event_listener_with_callback("mousedown", closure.as_ref().unchecked_ref())?;
@@ -44,14 +54,18 @@ pub fn start() -> Result<(), JsValue> {
     {
         let context = context.clone();
         let dragged = dragged.clone();
+        let target = target.clone();
         let w = w.clone();
+        let points = points.clone();
         let closure = Closure::wrap(Box::new(move |event: web_sys::MouseEvent| {
             if dragged.get() {
                 context.clear_rect(0.0, 0.0, 640.0, 480.0);
+                for p in points.borrow().iter() {
+                    context.fill_rect(p[0] - *w/2.0, p[1] - *w/2.0, *w, *w);
+                }
                 let mouse_x = event.offset_x() as f64;
                 let mouse_y = event.offset_y() as f64;
-                x.set(mouse_x);
-                y.set(mouse_y);
+                target.set([mouse_x, mouse_y]);
                 context.fill_rect(mouse_x - *w/2.0 , mouse_y - *w/2.0, *w, *w);
             }
         }) as Box<dyn FnMut(_)>);
@@ -60,8 +74,13 @@ pub fn start() -> Result<(), JsValue> {
     }
     {
         let dragged = dragged.clone();
-        let closure = Closure::wrap(Box::new(move |event: web_sys::MouseEvent| {
-            dragged.set(false);
+        let points = points.clone();
+        let target = target.clone();
+        let closure = Closure::wrap(Box::new(move |_event: web_sys::MouseEvent| {
+            if dragged.get() {
+                points.borrow_mut().push(target.get());
+                dragged.set(false);
+            }
         }) as Box<dyn FnMut(_)>);
         canvas.add_event_listener_with_callback("mouseup", closure.as_ref().unchecked_ref())?;
         closure.forget();
