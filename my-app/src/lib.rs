6e72ad294563fd1db16e9588ecc5eca0183e29b8
r-tezuka <ryuta.tezuka2@gmail.com>
2020-12-04 11:58:38 +0900

    Bezier作図を実装

--- my-app/src/lib.rs
+++ my-app/src/lib.rs
@@ -4,13 +4,15 @@ use wasm_bindgen::JsCast;
 
 #[wasm_bindgen(start)]
 pub fn start() -> Result<(), JsValue> {
+    let canvas_w = 640.0;
+    let canvas_h = 480.0;
     let document = web_sys::window().unwrap().document().unwrap();
     let canvas = document
         .create_element("canvas")?
         .dyn_into::<web_sys::HtmlCanvasElement>()?;
     document.body().unwrap().append_child(&canvas)?;
-    canvas.set_width(640);
-    canvas.set_height(480);
+    canvas.set_width(canvas_w as u32);
+    canvas.set_height(canvas_h as u32);
     canvas.style().set_property("border", "solid")?;
     let context = canvas
         .get_context("2d")?
@@ -18,34 +20,26 @@ pub fn start() -> Result<(), JsValue> {
         .dyn_into::<web_sys::CanvasRenderingContext2d>()?;
     let context = Rc::new(context);
     let dragged = Rc::new(Cell::new(false));
-    let target = Rc::new(Cell::new([0.0, 0.0]));
-    let point_width = 10.0;
-    let w = Rc::new(point_width);
-    let points = vec![[10.0, 10.0],[100.0, 10.0],[200.0, 10.0]];
-    for p in points.iter() {
-        context.fill_rect(p[0] - *w/2.0, p[1] - *w/2.0, *w, *w);
-    }
+    let w = 10.0; // width for point rect
+    let id: Option<usize> = None; // index of dragged point 
+    let id = Rc::new(Cell::new(id));
+    let points = vec![[10.0, 10.0],[100.0, 30.0],[200.0, 30.0],[300.0, 10.0]];
+    draw_bezier(points.clone(), w, context.clone());
     let points = Rc::new(RefCell::new(points));
     {
         let context = context.clone();
         let dragged = dragged.clone();
-        let w = w.clone();
+        let id = id.clone();
         let points = points.clone();
         let closure = Closure::wrap(Box::new(move |event: web_sys::MouseEvent| {
             context.begin_path();
             let mouse_x = event.offset_x() as f64;
             let mouse_y = event.offset_y() as f64;
-            let mut id: Option<usize> = None;
             for (i, p) in points.borrow().iter().enumerate() {
-                if mouse_x - *w/2.0 < p[0] && p[0] <  mouse_x + *w/2.0 && mouse_y - *w/2.0 < p[1] && p[1] < mouse_y + *w/2.0 {
+                if mouse_x - w/2.0 < p[0] && p[0] <  mouse_x + w/2.0 && mouse_y - w/2.0 < p[1] && p[1] < mouse_y + w/2.0 {
                     dragged.set(true);
-                    id = Some(i);
-                }else{
-                    context.fill_rect(p[0] - *w/2.0, p[1] - *w/2.0, *w, *w);
+                    id.set(Some(i));
                 }
-            }
-            if id != None {
-                points.borrow_mut().remove(id.unwrap());
             }
         }) as Box<dyn FnMut(_)>);
         canvas.add_event_listener_with_callback("mousedown", closure.as_ref().unchecked_ref())?;
@@ -54,19 +48,17 @@ pub fn start() -> Result<(), JsValue> {
     {
         let context = context.clone();
         let dragged = dragged.clone();
-        let target = target.clone();
-        let w = w.clone();
+        let id = id.clone();
         let points = points.clone();
         let closure = Closure::wrap(Box::new(move |event: web_sys::MouseEvent| {
             if dragged.get() {
-                context.clear_rect(0.0, 0.0, 640.0, 480.0);
-                for p in points.borrow().iter() {
-                    context.fill_rect(p[0] - *w/2.0, p[1] - *w/2.0, *w, *w);
-                }
+                context.clear_rect(0.0, 0.0, canvas_w, canvas_h);
                 let mouse_x = event.offset_x() as f64;
                 let mouse_y = event.offset_y() as f64;
-                target.set([mouse_x, mouse_y]);
-                context.fill_rect(mouse_x - *w/2.0 , mouse_y - *w/2.0, *w, *w);
+                if id.get() != None {
+                    points.borrow_mut()[id.get().unwrap()] = [mouse_x, mouse_y];
+                }
+                draw_bezier(points.borrow().to_vec(), w, context.clone());
             }
         }) as Box<dyn FnMut(_)>);
         canvas.add_event_listener_with_callback("mousemove", closure.as_ref().unchecked_ref())?;
@@ -74,11 +66,8 @@ pub fn start() -> Result<(), JsValue> {
     }
     {
         let dragged = dragged.clone();
-        let points = points.clone();
-        let target = target.clone();
         let closure = Closure::wrap(Box::new(move |_event: web_sys::MouseEvent| {
             if dragged.get() {
-                points.borrow_mut().push(target.get());
                 dragged.set(false);
             }
         }) as Box<dyn FnMut(_)>);
@@ -87,4 +76,22 @@ pub fn start() -> Result<(), JsValue> {
     }
 
     Ok(())
+}
+
+fn draw_bezier(points: Vec<[f64; 2]>, w: f64, context: Rc<web_sys::CanvasRenderingContext2d> ) {
+    for p in points.iter() {
+        context.fill_rect(p[0] - w/2.0, p[1] - w/2.0, w, w);
+    }
+    context.begin_path();
+    context.move_to(points[0][0],points[0][1]);
+    context.line_to(points[1][0],points[1][1]);
+    context.stroke();
+    context.begin_path();
+    context.move_to(points[0][0],points[0][1]);
+    context.bezier_curve_to(points[1][0],points[1][1],points[2][0],points[2][1],points[3][0],points[3][1]);
+    context.stroke();
+    context.begin_path();
+    context.move_to(points[3][0],points[3][1]);
+    context.line_to(points[2][0],points[2][1]);
+    context.stroke();
 }
